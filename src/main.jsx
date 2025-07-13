7b9b3b71528939d61544e0f40fd065fae4d250e2
Moose05 <musavictormnanzana@gmail.com>
2025-07-13 18:40:56 +0200

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 
